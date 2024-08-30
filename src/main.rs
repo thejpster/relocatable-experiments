bf9e7221a0943bee4c328982d40a32b6fc823a9f
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-08-30 10:25:37 +0100

    Tweaks and clarifications
    ) Added instructions for building the samples
    ) Set the entry point (removes a linker warning)
    ) Mark dead_code as allowed (removes some build warnings)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use core::sync::atomic::{AtomicU32, Ordering};
 
 #[used]
 #[no_mangle]
-#[link_section=".entry_point"]
+#[link_section = ".entry_point"]
 pub static ENTRY_POINT: extern "C" fn(&'static Api) -> i32 = entry_point;
 
 #[no_mangle]
@@ -14,45 +14,51 @@ static mut API: Option<&'static Api> = None;
 
 static PRINT_COUNTER: AtomicU32 = AtomicU32::new(0);
 
-trait Printable {
+pub trait Printable {
     fn print(&self, number: u32);
-	fn sides(&self) -> u32;
+    fn sides(&self) -> u32;
 }
 
-struct Square {
-   width: i32,
+#[allow(dead_code)]
+pub struct Square {
+    width: i32,
 }
 
 impl Printable for Square {
-	#[inline(never)]
+    #[inline(never)]
     fn print(&self, number: u32) {
-		let api = unsafe { API.unwrap() };
-		(api.puts)("Square".as_ptr(), number as usize);
-		PRINT_COUNTER.store(PRINT_COUNTER.load(Ordering::Relaxed) + 1, Ordering::Relaxed);
+        let api = unsafe { API.unwrap() };
+        (api.puts)("Square".as_ptr(), number as usize);
+        PRINT_COUNTER.store(PRINT_COUNTER.load(Ordering::Relaxed) + 1, Ordering::Relaxed);
     }
 
-	fn sides(&self) -> u32 { 4 }
+    fn sides(&self) -> u32 {
+        4
+    }
 }
 
-struct Circle {
-   radius: i32
+#[allow(dead_code)]
+pub struct Circle {
+    radius: i32,
 }
 
 impl Printable for Circle {
-	#[inline(never)]
+    #[inline(never)]
     fn print(&self, number: u32) {
-		let api = unsafe { API.unwrap() };
-		(api.puts)("Circle".as_ptr(), number as usize);         
-		PRINT_COUNTER.store(PRINT_COUNTER.load(Ordering::Relaxed) + 1, Ordering::Relaxed);
+        let api = unsafe { API.unwrap() };
+        (api.puts)("Circle".as_ptr(), number as usize);
+        PRINT_COUNTER.store(PRINT_COUNTER.load(Ordering::Relaxed) + 1, Ordering::Relaxed);
     }
 
-	fn sides(&self) -> u32 { 0xFFFFFFFF }
+    fn sides(&self) -> u32 {
+        0xFFFFFFFF
+    }
 }
 
 #[inline(never)]
-fn print(item: &dyn Printable) {
-	let x = item.sides();
-	item.print(x)
+pub fn print(item: &dyn Printable) {
+    let x = item.sides();
+    item.print(x)
 }
 
 #[repr(C)]
@@ -60,33 +66,35 @@ pub struct Api {
     puts: fn(data: *const u8, len: usize) -> i32,
 }
 
+#[no_mangle]
 extern "C" fn entry_point(api: &'static Api) -> i32 {
-	unsafe { API = Some(api); }
-	let message = "Hello, world!\n";
-	let x = testlib::test() as usize;
-	(api.puts)(message.as_ptr(), x);
+    unsafe {
+        API = Some(api);
+    }
+    let message = "Hello, world!\n";
+    let x = testlib::test() as usize;
+    (api.puts)(message.as_ptr(), x);
 
-	unsafe {
-		static mut BIG_BUFFER: [u8; 64] = [0xCC; 64];
-		BIG_BUFFER[0] = 0xAA;
-		BIG_BUFFER[1] = 0xBB;
-		BIG_BUFFER[2] = 0xCC;
-		BIG_BUFFER[3] = 0xDD;
-		(api.puts)(BIG_BUFFER.as_ptr(), 64);
-	}
+    unsafe {
+        static mut BIG_BUFFER: [u8; 64] = [0xCC; 64];
+        BIG_BUFFER[0] = 0xAA;
+        BIG_BUFFER[1] = 0xBB;
+        BIG_BUFFER[2] = 0xCC;
+        BIG_BUFFER[3] = 0xDD;
+        (api.puts)(BIG_BUFFER.as_ptr(), 64);
+    }
 
-	let square = Square { width: 5 };
+    let square = Square { width: 5 };
 
-	print(&square);
+    print(&square);
 
-	let circle = Circle { radius: 10 };
+    let circle = Circle { radius: 10 };
 
-	print(&circle);
-	0
+    print(&circle);
+    0
 }
 
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-	loop {
- 	}
+    loop {}
 }
